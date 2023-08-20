b9046ca3f25dfbfc3247ff1b1d9420a11c61fe47
mahabubulislam <mahabub2k1@gmail.com>
2023-08-20 16:53:21 +0600

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import Layout from '@/components/Layout/Layout'
+import Footer from '@/components/Shared/Footer'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Footer />
+    </Layout>
+  )
 }
